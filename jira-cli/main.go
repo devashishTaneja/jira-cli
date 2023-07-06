63763a00b6e7e95ce6d05b27cab873304bdf2e64
Devashish Taneja <dtaneja@enphaseenergy.com>
2023-07-06 12:27:14 +0530

    Move credentials to env

--- jira-cli/main.go
+++ jira-cli/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"encoding/base64"
 	"encoding/json"
 	"fmt"
+	"github.com/savioxavier/termlink"
 	"log"
 	"net/http"
 	"os"
@@ -11,7 +13,7 @@ import (
 )
 
 type issue struct {
-	Id, Key string
+	SummaryText, Key string
 }
 
 type section struct {
@@ -49,14 +51,16 @@ func main() {
 		os.Exit(1)
 	}
 
-	jiraDomain := "https://enphase.atlassian.net"
-	jiraBasicAuth := "Basic " + os.Getenv("jira_auth_token")
-	jiraBasicAuth = "Basic ZHRhbmVqYUBlbnBoYXNlZW5lcmd5LmNvbTpBVEFUVDN4RmZHRjAxU1BYMDhWc1ItNUFIa054WlVrMENuMEdmRG1XN3UtSk5rTUhXX2paQ1V4YVpfZVBMeE9PZzR0N1RvNDJPWVB1VnhuZnlzQ0pvYlVPM0M2RkdJUG5EQl9PVUxsYWJ0aWUxNkNlUGY2Z0MyeGpWYmlzSnhnUDBMTWViejFoekktSFQzMGVvRlY2WEVrUGVCamlxcENia3hleVRoRmpteExTWExHeHF4emZVOVU9MUEyMEJBNkE="
+	jiraDomain := os.Getenv("JIRA_DOMAIN")
+	username := os.Getenv("JIRA_API_USER")
+	apikey := os.Getenv("JIRA_API_KEY")
+	base64Auth := "Basic " + base64.StdEncoding.EncodeToString([]byte(username + ":" + apikey))
+
 	queryUrl := jiraDomain + "/rest/api/3/issue/picker?query=" + os.Args[1]
 
 	client := http.Client{}
 	req, err := http.NewRequest("GET", queryUrl, nil)
-	req.Header.Add("Authorization", jiraBasicAuth)
+	req.Header.Add("Authorization", base64Auth)
 	req.Header.Add("Accept", "application/json")
 	req.Header.Add("Content-Type", "application/json")
 	resp, err := client.Do(req)
@@ -78,7 +82,12 @@ func main() {
 
 	if len > 1 {
 		for i := 0; i < len; i++ {
-			fmt.Println(jiraDomain + "/browse/" + issues[i].Key)
+			fmt.Println("______________________________________________")
+			issue := issues[i]
+			fmt.Println(issue.SummaryText)
+			jiraLink := jiraDomain + "/browse/" + issue.Key
+			fmt.Println(termlink.ColorLink(issue.Key, jiraLink, "red"))
+
 		}
 	} else if len == 0 {
 		fmt.Println("No related issues found")
